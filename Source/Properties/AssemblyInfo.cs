5d4bf4a6d4da0d75a2ca44f8b3359ab700c158fb
siimav <siim.aaver@gmail.com>
2024-07-23 15:44:25 +0300

    Update assembly version on build

--- Source/Properties/AssemblyInfo.cs
+++ Source/Properties/AssemblyInfo.cs
@@ -1,3 +1,4 @@
+#define CIBUILD_disabled
 using System.Reflection;
 using System.Runtime.CompilerServices;
 
@@ -15,8 +16,10 @@ using System.Runtime.CompilerServices;
 // The form "{Major}.{Minor}.*" will automatically update the build and revision,
 // and "{Major}.{Minor}.{Build}.*" will update just the revision.
 [assembly: AssemblyVersion ("2.0.0.0")]
-// The following attributes are used to specify the signing key for the assembly, 
-// if desired. See the Mono documentation for more information about signing.
-//[assembly: AssemblyDelaySign(false)]
-//[assembly: AssemblyKeyFile("")]
-
+#if CIBUILD
+[assembly: AssemblyFileVersion("@MAJOR@.@MINOR@.@PATCH@.@BUILD@")]
+[assembly: KSPAssembly("KSCSwitcher", @MAJOR@, @MINOR@)]
+#else
+[assembly: AssemblyFileVersion("2.99.0.0")]
+[assembly: KSPAssembly("KSCSwitcher", 2, 99)]
+#endif
